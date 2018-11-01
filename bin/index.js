780b8fae85eb02c6aa34d0bb746108d4b8c22cd3
Asa Rudick <arudick@metromile.com>
2018-11-01 16:59:54 -0700

    try passing options directly

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,18 @@
 const appRootPath = require('app-root-path');
 const path = require('path');
 
+const options = {
+  compilerOptions: {
+    lib: ['es2015', 'es2016', 'es2017'],
+    allowSyntheticDefaultImports: true,
+    module: 'commonjs',
+    moduleResolution: 'node',
+    skipLibCheck: true,
+    experimentalDecorators: true,
+    emitDecoratorMetadata: true,
+  },
+};
+
 // All typescript from here.
-require('ts-node').register({
-  project: path.join(appRootPath.toString(), 'tsconfig.json'),
-});
-console.log(path.join(appRootPath.toString(), 'tsconfig.json'));
+require('ts-node').register(options);
 require('./sasquatch');
