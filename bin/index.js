d8d4f392dfd0f7cc8fc429b7f4d6ae6a9233a550
Asa Rudick <arudick@metromile.com>
2018-11-01 16:37:06 -0700

    correct reference to tsconfig location + version bump

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
+const path = require('path');
 
 // All typescript from here.
-require('ts-node/register');
+require('ts-node').register({
+  project: path.join(__dirname, '../tsconfig.json'),
+});
 require('./sasquatch');
