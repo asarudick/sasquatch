5e46551aecb2f149830ea38708f77dcaf264e09d
Asa Rudick <arudick@metromile.com>
2018-11-18 22:39:45 -0800

    shorten readSection function

--- src/reporting/readSection.ts
+++ src/reporting/readSection.ts
@@ -2,16 +2,12 @@ import * as fs from 'fs';
 const bufferSize = 64;
 
 export function readSection(path, start, end) {
-  let file;
-  let buffer;
+  const buffer = Buffer.alloc(bufferSize, '');
   let currentLine = 1;
   const lines = [];
 
   // Open file synchronously
-  try {
-    file = fs.openSync(path, 'r');
-    buffer = Buffer.alloc(bufferSize, '');
-  } catch (e) {}
+  const file = fs.openSync(path, 'r');
 
   while (currentLine <= end) {
     fs.readSync(file, buffer, 0, bufferSize, null);
@@ -20,9 +16,7 @@ export function readSection(path, start, end) {
 
     // Append end of last string segment.
     if (lines.length) {
-      lines[lines.length - 1] += bufferLines[0];
-      bufferLines.shift();
-
+      lines[lines.length - 1] += bufferLines.shift();
       // Previous segment turned into a line.
       currentLine++;
     }
