d45185b46e9d254fb00581893174f960bafe70b4
Asa Rudick <arudick@metromile.com>
2019-04-05 14:55:14 -0700

    add declaration file for the only js file in project

---
+++ src/default.config.d.ts
@@ -0,0 +1,11 @@
+import { IndentationText, QuoteKind } from 'ts-morph';
+declare const _default: {
+  options: {
+    manipulationSettings: {
+      indentationText: IndentationText;
+      quoteKind: QuoteKind;
+    };
+  };
+  modules: import('./types/Module').Module[];
+};
+export default _default;
